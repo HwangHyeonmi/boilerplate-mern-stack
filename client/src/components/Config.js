87fdf7ce0e08487ceb1868b9fdc26c2de80d54f1
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2020-12-07 20:14:09 +0900

    dd

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY ="9dc405a51a678419098e700b69672678";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
